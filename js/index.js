a4e981b2fe2afe5b4ec1f1cfd3f93a1a93061df4
Sergio1902 <ser19car@gmail.com>
2021-01-21 09:10:43 -0500

    RESPONSIVE

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 import btnHamburguesa from './hamburguesa.js'
-document.addEventListener("DOMContentLoaded", function (e) {
- 
-    btnHamburguesa(".btn-hamburguesa", ".menu-flotante", ".menu-flotante a")
 
+document.addEventListener("DOMContentLoaded", function (e) {
+    btnHamburguesa(".btn-hamburguesa", ".menu-flotante", ".menu-flotante a")
 })
