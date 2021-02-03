9ca0dfb18d209f48cc8fecc287b2469622166b90
Sergio1902 <ser19car@gmail.com>
2021-02-02 19:58:13 -0500

    MEDIA-QUERY

--- js/hamburguesa.js
+++ js/hamburguesa.js
@@ -1,6 +1,5 @@
 export default function btnHamburguesa(btn,menu,a) {
     
-
   document.addEventListener("click", function (e) {
         if(e.target.matches(btn) || e.target.matches(`${btn} *`)){
             document.querySelector(menu).classList.toggle("is-active")
@@ -9,10 +8,8 @@ export default function btnHamburguesa(btn,menu,a) {
         if(e.target.matches(a)){
             document.querySelector(menu).classList.remove("is-active")
             document.querySelector(btn).classList.remove("is-active")
-
         }
-
     })
-
+    
 
 }
